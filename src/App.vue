3eff12bfc176bf3ef3308d58f9e703abcd88d251
Gasilov Danil <gasilov.d@roistat.com>
2025-04-16 12:01:51 +0300

    verstka

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <h3>Title</h1>
+  <h3>Title</h3>
+  <p>Anons</p>
 </template>
 
 <script>
-
+export default {
+  
+}
 </script>
 
 <style scoped>
-h1{
+h3{
+  font-weight: lighter,
+}
+
+p{
   color: red
 }
 </style>
